e1209a394b71876613ec7c9dcff8294264673d3e
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2021-02-05 20:43:29 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,60 @@ import BanjaLuka from "./gradovi/BanjaLuka";
 import Tuzla from "./gradovi/Tuzla";
 import Zenica from "./gradovi/Zenica";
 import Bijeljina from "./gradovi/Bijeljina";
-
-
-
+import Cazin from "./gradovi/Cazin";
+import Doboj from "./gradovi/Doboj";
+import Mostar from "./gradovi/Mostar";
+import Prijedor from "./gradovi/Prijedor";
+import Zvornik from "./gradovi/Zvornik";
+import Zivinice from "./gradovi/Zivinice";
+import Bihac from "./gradovi/Bihac";
+import Visoko from "./gradovi/Visoko";
 
 const App = (props) => {
   return (
     <Router>
       <>
         <Switch>
+        <Route path="/zvornik">
+            <Header/>
+            <Current/>
+            <Zvornik/>
+          </Route>
+        <Route path="/zivinice">
+            <Header/>
+            <Current/>
+            <Zivinice/>
+          </Route>
+          <Route path="/bihac">
+            <Header/>
+            <Current/>
+            <Bihac/>
+          </Route>
+          <Route path="/visoko">
+            <Header/>
+            <Current/>
+            <Visoko/>
+          </Route>
+        <Route path="/cazin">
+            <Header/>
+            <Current/>
+            <Cazin/>
+          </Route>
+        <Route path="/doboj">
+            <Header/>
+            <Current/>
+            <Doboj/>
+          </Route>
+          <Route path="/mostar">
+            <Header/>
+            <Current/>
+            <Mostar/>
+          </Route>
+          <Route path="/prijedor">
+            <Header/>
+            <Current/>
+            <Prijedor/>
+          </Route>
         <Route path="/bijeljina">
             <Header/>
             <Current/>
